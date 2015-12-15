8f3a2c80efd46c59f4706662009b3cb46b67bf34
upa8 <pratikupacharya@gmail.com>
2015-12-15 12:33:30 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Polaris</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+	<div class = "row">
+		<div class="col-sm-4"></div>
+
+		<div class="col-sm-4">
+			<center>
+				
+				<h1> Comming Soon  !!!</h1>
+				<br>
+				<br>
+				<img src="img/logo.jpg" class="img-rounded" alt="Cinque Terre" width="304" height="236"> 
+				<br>
+				<br>
+				<p>	Under Construction .</p>
+
+			</center>
+		</div>
+
+		<div class="col-sm-4"></div>
+	</div>
+  
+</div>
+
+</body>
+</html>
