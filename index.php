77a277c03601a16b2942d0661d1d973271d609cf
upa8 <pratikupacharya@gmail.com>
2016-01-01 00:33:03 +0530

    Basic structure implemented

--- index.php
+++ index.php
@@ -1,36 +1,306 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>Polaris</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<!doctype html>
+<html>
+<head lang="en">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	
+	<title>Polaris1k16</title> 
+	<meta name="description" content="Polaris2K16" />	    
+	<meta name="keywords" content="Polaris, RCOEM, Ramdeobaba College Of Engineering, Polaris2k16, Technical Fest, ">
+	<meta property="og:title" content="">
+
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="fancybox/jquery.fancybox-v=2.1.5.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/font-awesome.min.css" rel="stylesheet">
+	
+	<link rel="stylesheet" type="text/css" href="css/style.css">	
+	
+	<link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,600,300,200&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+	<link rel="prefetch" href="images/zoom.png">
 </head>
 <body>
+	<div class="navbar navbar-fixed-top" data-activeslide="1">
+		<div class="container">
+			<!-- .navbar-toggle is used as the toggle for collapsed navbar content -->
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			
+			
+			<div class="nav-collapse collapse navbar-responsive-collapse">
+				<ul class="nav row">
+					<li data-slide="1" class="col-12 col-sm-2"><a id="menu-link-1" href="#slide-1" title="Next Section"><span class="icon icon-home"></span> <span class="text">HOME</span></a></li>
+					<li data-slide="2" class="col-12 col-sm-2"><a id="menu-link-2" href="#slide-2" title="Next Section"><span class="icon icon-user"></span> <span class="text">ABOUT US</span></a></li>
+					<li data-slide="3" class="col-12 col-sm-2"><a id="menu-link-3" href="#slide-3" title="Next Section"><span class="icon icon-briefcase"></span> <span class="text">EVENTS</span></a></li>
+					<li data-slide="4" class="col-12 col-sm-2"><a id="menu-link-4" href="#slide-4" title="Next Section"><span class="icon icon-gears"></span> <span class="text">WORKSHOPS</span></a></li>
+					<li data-slide="5" class="col-12 col-sm-2"><a id="menu-link-5" href="#slide-5" title="Next Section"><span class="icon icon-heart"></span> <span class="text">GALLERY</span></a></li>
+					<li data-slide="6" class="col-12 col-sm-2"><a id="menu-link-6" href="#slide-6" title="Next Section"><span class="icon icon-envelope"></span> <span class="text">CONTACT</span></a></li>
+				</ul>
+				<div class="row">
+					<div class="col-sm-2 active-menu"></div>
+				</div>
+			</div><!-- /.nav-collapse -->
+		</div><!-- /.container -->
+	</div><!-- /.navbar -->
+	
+	
+	<!-- === Arrows === -->
+	<div id="arrows">
+		<div id="arrow-up" class="disabled"></div>
+		<div id="arrow-down"></div>
+		<div id="arrow-left" class="disabled visible-lg"></div>
+		<div id="arrow-right" class="disabled visible-lg"></div>
+	</div><!-- /.arrows -->
+	
+	
+	<!-- === MAIN Background === -->
+	<div class="slide story" id="slide-1" data-slide="1">
+		<div class="container">
+			<div id="home-row-1" class="row clearfix">
+				<div class="col-12">
+					<h1 class="font-semibold">POLARIS <span class="font-thin">2K16</span></h1>
+					<h4 class="font-thin">Annual technical festival of <span class="font-semibold">COMPUTER SCIECNCE AND ENGINEERING  </span> RCOEM</h4>
+					<br>
+					<br>
+				</div><!-- /col-12 -->
+			</div><!-- /row -->
 
-<div class="container">
-	<div class = "row">
-		<div class="col-sm-4"></div>
+			<div id="home-row-2" class="row clearfix">
+				<div class="col-12 col-sm-4">
+					<div class="home-hover navigation-slide" data-slide="4">
+						<a href="">	
+							<img src="images/s02.png">
+						</a>
+					</div>
+					<span>
+						PRESENTATION
+					</span>
+				</div>
+				<div class="col-12 col-sm-4">
+					<div class="home-hover navigation-slide" data-slide="3">
+						<img src="images/s01.png">
+					</div>
+					<span>
+						VIDEO
+					</span>
+				</div>
+				<div class="col-12 col-sm-4">
+					<div class="home-hover navigation-slide" data-slide="5">
+						<img src="images/s03.png">
+					</div>
+					<span>
+						GALLERY
+					</span>
+				</div>
+			</div><!-- /row -->
 
-		<div class="col-sm-4">
+		</div><!-- /container -->
+	</div><!-- /slide1 -->
+	
+	<!-- === Slide 2 === -->
+	<div class="slide story" id="slide-2" data-slide="2">
+		<div class="container">
+			<div class="row title-row">
+				<div class="col-12 font-thin">This is , <span class="font-semibold">National Technical Event </span> Organized in RCOEM NAGPUR</div>
+			</div><!-- /row -->
+			<div class="row line-row">
+				<div class="hr">&nbsp;</div>
+			</div><!-- /row -->
+			<div class="row subtitle-row">
+				<div class="col-12 font-thin">Following are key  <span class="font-semibold">points </span></div>
+			</div><!-- /row -->
+			<div class="row content-row">
+				<div class="col-12 col-lg-3 col-sm-6">
+					<p><i class="icon icon-eye-open"></i></p>
+					<h2 class="font-thin">Intelligent <span class="font-semibold">Lectures</span></h2>
+					<h4 class="font-thin">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h4>
+				</div><!-- /col12 -->
+				<div class="col-12 col-lg-3 col-sm-6">
+					<p><i class="icon icon-laptop"></i></p>
+					<h2 class="font-thin">CODING <span class="font-semibold">EVENTS</span></h2>
+					<h4 class="font-thin">Pair coding event</h4>
+				</div><!-- /col12 -->
+				<div class="col-12 col-lg-3 col-sm-6">
+					<p><i class="icon icon-tablet"></i></p>
+					<h2 class="font-thin">Latest  <span class="font-semibold">Technology</span></h2>
+					<h4 class="font-thin">You will get to know about latest technology</h4>
+				</div><!-- /col12 -->
+				<div class="col-12 col-lg-3 col-sm-6">
+					<p><i class="icon icon-pencil"></i></p>
+					<h2 class="font-semibold">Fun and Enjoyment  </h2>
+					<h4 class="font-thin">DJ Nights etc </h4>
+				</div><!-- /col12 -->
+			</div><!-- /row -->
+		</div><!-- /container -->
+	</div><!-- /slide2 -->
+	
+	<!-- EVENT SHOWCASE -->
+	<!-- === SLide 3 - Portfolio -->
+	<div class="slide story" id="slide-3" data-slide="3">
+		<div class ="row">
 			<center>
-				
-				<h1> Comming Soon  !!!</h1>
-				<br>
-				<br>
-				<img src="img/logo.jpg" class="img-rounded" alt="Cinque Terre" width="304" height="236"> 
-				<br>
-				<br>
-				<p>	Under Construction .</p>
-
+				 <h1><span class="font-semibold">EVENTS</span></h1>
 			</center>
-		</div>
 
-		<div class="col-sm-4"></div>
-	</div>
- 
-</div>
+		</div>
+		<div class="row">			
+			<div class="col-12 col-sm-6 col-lg-2">
+			    <a data-fancybox-group="portfolio" class="fancybox" href="images/portfolio/p03-large.jpg">
+			        <img class="thumb" src="images/portfolio/p03-small.jpg" alt=""></img>
+			    </a>
+			</div>
+			<div class="col-12 col-sm-6 col-lg-2">
+			    <a data-fancybox-group="portfolio" class="fancybox" href="images/portfolio/p04-large.jpg">
+			        <img class="thumb" src="images/portfolio/p04-small.jpg" alt=""></img>
+			    </a>
+			</div>
+			<div class="col-12 col-sm-6 col-lg-2">
+			    <a data-fancybox-group="portfolio" class="fancybox" href="images/portfolio/p03-large.jpg">
+			        <img class="thumb" src="images/portfolio/p03-small.jpg" alt=""></img>
+			    </a>
+			</div>
+			
+			<div class="col-12 col-sm-6 col-lg-2">
+			    <a data-fancybox-group="portfolio" class="fancybox" href="images/portfolio/p04-large.jpg">
+			        <img class="thumb" src="images/portfolio/p04-small.jpg" alt=""></img>
+			    </a>
+			</div>
+			<div class="col-12 col-sm-6 col-lg-2">
+			    <a data-fancybox-group="portfolio" class="fancybox" href="images/portfolio/p03-large.jpg">
+			        <img class="thumb" src="images/portfolio/p03-small.jpg" alt=""></img>
+			    </a>
+			</div>
+			
+			<div class="col-12 col-sm-6 col-lg-2">
+			    <a data-fancybox-group="portfolio" class="fancybox" href="images/portfolio/p04-large.jpg">
+			        <img class="thumb" src="images/portfolio/p04-small.jpg" alt=""></img>
+			    </a>
+			</div>
+			<div class="col-12 col-sm-6 col-lg-2">
+			    <a data-fancybox-group="portfolio" class="fancybox" href="images/portfolio/p04-large.jpg">
+			        <img class="thumb" src="images/portfolio/p04-small.jpg" alt=""></img>
+			    </a>
+			</div>
+			<div class="col-12 col-sm-6 col-lg-2">
+			    <a data-fancybox-group="portfolio" class="fancybox" href="images/portfolio/p03-large.jpg">
+			        <img class="thumb" src="images/portfolio/p03-small.jpg" alt=""></img>
+			    </a>
+			</div>
+			
+		</div><!-- /row -->
+	</div><!-- /slide3 -->
+	
+	<!-- === Slide 4 - Process === -->
+	<div class="slide story" id="slide-4" data-slide="4">
+		<div class="container">
+			<div class="row title-row">
+				<div class="col-12 font-thin"> <span class="font-semibold">WORKSHOPS</span></div>
+			</div><!-- /row -->
+			<div class="row line-row">
+				<div class="hr">&nbsp;</div>
+			</div><!-- /row -->
+			<div class="row subtitle-row">
+				<div class="col-sm-1 hidden-sm">&nbsp;</div>
+				<div class="col-12 col-sm-10 font-light">There are many workshops we are offering . 
+					This workshops are as follows.</div>
+				<div class="col-sm-1 hidden-sm">&nbsp;</div>
+			</div><!-- /row -->
+			<div class="row content-row">
+				<div class="col-12 col-sm-4">
+					<p><i class="icon icon-cog"></i></p>
+					<h2 class="font-thin"><br><span class="font-semibold">Linux</span></h2>
+					<h4 class="font-thin">Linux Operation sytem workshop with python scripting .</h4>
+				</div><!-- /col12 -->
+				<div class="col-12 col-sm-4">
+					<p><i class="icon icon-cloud"></i></p>
+					<h2 class="font-thin"><br><span class="font-semibold">Sales force</span></h2>
+					<h4 class="font-thin">Cloud Computing Workshop</h4>
+				</div><!-- /col12 -->
+				<div class="col-12 col-sm-4">
+					<p><i class="icon icon-laptop"></i></p>
+					<h2 class="font-thin"><br><span class="font-semibold">Coding Ethics</span></h2>
+					<h4 class="font-thin">Industry level coding standard information.</h4>
+				</div><!-- /col12 -->
+				
+			</div><!-- /row -->
+		</div><!-- /container -->
+	</div><!-- /slide4 -->
+	
+	<!-- === Slide 5 === -->
+	<div class="slide story" id="slide-5" data-slide="5">
+		<div class="container">
+			<div class="row title-row">
+				<div class="col-12 font-thin"><span class="font-semibold">Sponsers</span> </div>
+			</div><!-- /row -->
+			<div class="row line-row">
+				<div class="hr">&nbsp;</div>
+			</div><!-- /row -->
+			<div class="row subtitle-row">
+				<div class="col-sm-1 hidden-sm">&nbsp;</div>
+				<div class="col-12 col-sm-10 font-light">We are greatful for following sponsers </div>
+				<div class="col-sm-1 hidden-sm">&nbsp;</div>
+			</div><!-- /row -->
+			<div class="row content-row">
+				<div class="col-1 col-sm-1 hidden-sm">&nbsp;</div>
+				<div class="col-12 col-sm-2"><img src="images/client01.png" alt=""></div>
+				<div class="col-12 col-sm-2"><img src="images/client02.png" alt=""></div>
+				<div class="col-12 col-sm-2"><img src="images/client03.png" alt=""></div>
+				<div class="col-12 col-sm-2"><img src="images/client04.png" alt=""></div>
+				<div class="col-12 col-sm-2"><img src="images/client05.png" alt=""></div>
+				<div class="col-1 col-sm-1 hidden-sm">&nbsp;</div>
+			</div><!-- /row -->
+		</div><!-- /container -->
+	</div><!-- /slide5 -->
+	
+	<!-- === Slide 6 / Contact === -->
+	<div class="slide story" id="slide-6" data-slide="6">
+		
+		<div class="container">
+			<div class="row title-row">
+				<div class="col-12 font-light"><span class="font-semibold">CONTACT</span></div>
+			</div><!-- /row -->
+			<div class="row line-row">
+				<div class="hr">&nbsp;</div>
+			</div><!-- /row -->
+			<div class="row subtitle-row">
+				<div class="col-sm-1 hidden-sm">&nbsp;</div>
+				<div class="col-12 col-sm-10 font-light">Under Construction!!!</div>
+				<div class="col-sm-1 hidden-sm">&nbsp;</div>
+			</div><!-- /row -->
+			<div id="contact-row-4" class="row">
+			</div><!-- /row -->
+		</div><!-- /container -->
+	</div><!-- /Slide 6 -->
+	
 </body>
+
+	<!-- SCRIPTS -->
+	<script src="js/html5shiv.js"></script>
+	<script src="js/jquery-1.10.2.min.js"></script>
+	<script src="js/jquery-migrate-1.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.easing.1.3.js"></script>
+	<script type="text/javascript" src="fancybox/jquery.fancybox.pack-v=2.1.5.js"></script>
+	<script src="js/script.js"></script>
+	
+	<!-- fancybox init -->
+	<script>
+	$(document).ready(function(e) {
+		var lis = $('.nav > li');
+		menu_focus( lis[0], 1 );
+		
+		$(".fancybox").fancybox({
+			padding: 10,
+			helpers: {
+				overlay: {
+					locked: false
+				}
+			}
+		});
+	
+	});
+	</script>
+
 </html>
