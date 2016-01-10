9acc4c33bd05cbcb6720c23fe3f472bcc634cd8c
upa8 <pratikupacharya@gmail.com>
2016-01-11 00:36:39 +0530

    College page has been formated

--- tools/views/college.php
+++ tools/views/college.php
@@ -1,4 +1,5 @@
 <?php 
+	// This is for pagination of the page 
 	$start=0;
     $limit=10;
     $page_id = 0;
@@ -7,6 +8,13 @@
         $page_id=$_GET['page_id'];
         $start=($page_id-1)*$limit;
     }
+?>
+<?	
+	// This is controller coder of our page 
+	// For college page , it will be different than 
+	// our event page   
+	// We can use this in a page and add that page 
+	// in this file to avoid redundancy 
 	if (isset($_POST["formtype"])) {
 		 $type = $_POST["formtype"];
 		 switch ($type) {
@@ -29,128 +37,163 @@
 		 		# code...
 		 		break;
 		 }
-     } 
+     }
+     //
+
 ?>
-<?php include("views/_main_header.php");?>
-<hr>
-<div class="row">
-	<div class="col-sm-11">
-	</div>
-	<div class="col-sm-1">
-		<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addEventStudent">Add</button>
-	</div>
-</div>
-<div class="row">
-	<div class="table-responsive">
-		<table class="table">
-		    <thead>
-		      <tr>
-		        <th>Id</th>
-		        <th>Name</th>
-		        <th>Mobile</th>
-		        <th>Cost</th>
-		        <th>Action</th>
-		      </tr>
-		    </thead>
-			<!-- Add pagination as well as -->
-			<tbody>
-			<?php
-				$rowCount = $Polaris->getTotalEntries();
-				$total = ceil($rowCount/$limit);
-				$query = $Polaris->getSelectedEventStudentData($page_id);
-				$count = ($page_id - 1)*10 + 1;
-				while($result = $query->fetchObject()){
-					$color = $count % 3;
-					switch ($color) {
-						case 0:
-							# code...
-							echo '<tr class="success">';
-							break;
-						case 1:
-							# code...
-							echo '<tr class="active">';
-							break;
-						case 2:
-							# code...
-							echo '<tr class="infor">';
-							break;
-						default:
-							# code...
-							break;
-					}
-					// Variable that we are not using here are 
-					// $result->eemail 
 
-					echo '<td>'.$count.'</td>
-						        <td>'.$result->ename.'</td>
-						        <td>'.$result->emobile.'</td>
-						        <td>'.$result->ecost.'</td>
-						        <td>
+<?php 
+	
+	include("views/_main_header.php");
 
-						        	 <form role="form" action="events.php" method="POST">
+?>
+	<hr>
+	<!-- A row starts here -->
+	<div class="row">
+		<div class="col-sm-11">
+		</div>
+		<div class="col-sm-1">
+			<button type="button" class="btn btn-primary" data-toggle="modal" 
+					data-target="#addEventStudent">Add</button>
+		</div>
+	</div>
+	<!-- A row ends here -->
+
+	<!-- A row starts here -->
+	<div class="row">
+  		<!-- Table responsive div starts here -->
+		<div class="table-responsive">
+			<table class="table">
+		    	<thead>
+		      		<tr>
+		        		<th>Id</th>
+				        <th>Name</th>
+				        <th>Year</th>
+				        <th>Shift</th>
+				        <th>Status</th>
+				        <th>Action</th>
+		      		</tr>
+		    	</thead>
+				<!-- Add pagination as well as -->
+				<tbody>
+				<?php
+						//Here get the entries of branch
+						// So pass parameter as a branch in this 
+						// following function 
+						$rowCount = $Polaris->getTotalEntries();
+						$total = ceil($rowCount/$limit);
+						$count = ($page_id - 1)*10 + 1;
+						
+						// Get the event details of the brnach student 
+						// So pass branch as a parameter in this function
+						// Directly get an array a result from this function
+						// So that we could just display that as a views  
+						$query = $Polaris->getSelectedEventStudentData($page_id);
+						// 
+						while($result = $query->fetchObject()){
+								
+								$color = $count % 3;
+								
+								switch ($color) {
+										case 0:
+											# code...
+											echo '<tr class="success">';
+											break;
+										case 1:
+											# code...
+											echo '<tr class="active">';
+											break;
+										case 2:
+											# code...
+											echo '<tr class="infor">';
+											break;
+										default:
+											# code...
+											break;
+								}
+							
+							// Now change this variables into the branch and shift and year 
+
+							echo '<td>'.$count.'</td>
+						          <td>'.$result->ename.'</td>
+						          <td>'.$result->emobile.'</td>
+						          <td>'.$result->ecost.'</td>
+						          <td>
+						        	 <form role="form" action="college.php" method="POST">
 						                  <div class="form-group">
 						                    <input type="hidden" class="form-control" id="edeleteId" name="edeleteId" value='.$result->eid.'>
 						                  </div>
 						                  <div class="form-group">
 						                    <input type="hidden" class="form-control" id="formtype" name="formtype" value=2>
 						                  </div>
-						                  ';
-						               	  echo " <button type=\"button\" onclick='updateEventStudentInfoModal(".$result->eid.",\"".$result->ename."\",\"".$result->emobile."\",\"".$result->eemail."\",\"".$result->ecost."\")' class=\"btn btn-info\" data-toggle=\"modal\" data-target=\"#eventStudentInfo\">Info</button>
-						  	           			";
-						               echo '	
-						                 		             
-                  							<button type="submit" class="btn btn-danger">Delete</button>
+						          ';
+						    
+						    echo " <button type=\"button\" onclick='updateEventStudentInfoModal(".$result->eid.",\"".$result->ename."\",\"".$result->emobile."\",\"".$result->eemail."\",\"".$result->ecost."\")' class=\"btn btn-info\" data-toggle=\"modal\" data-target=\"#eventStudentInfo\">Info</button>
+						  	     ";
+						    echo '	<button type="submit" class="btn btn-danger">Delete</button>
                  					</form>
-						        
-						        </td>
-						      </tr>
-						';
-					$count++;
-				}		
-			?>	   
-			</tbody> 
-	  </table>
-  <div>
-  <center>
-	  <ul class="pagination">
-	  	<?php 
-	  	if($page_id>1)
-                {
-                  echo "<a href='?page_id=".($page_id-1)."' class='button'>PREVIOUS</a>";
-                }
-                if($page_id!=$total)
-                {
-                  echo "<a href='?page_id=".($page_id+1)."' class='button'>NEXT</a>";
-                }
+							        </td>
+	        					    </tr>
+								 ';
+							$count++;
+						}		
+				?>	   
+				</tbody> 
+	  		</table>
+  		<div>
+  		<!-- Table responsive div ends here -->
+  		<!-- Center tag starts here -->
+  		<center>
+	  		<ul class="pagination">
+	  			<?php 
+	  				if($page_id>1){
+                  		echo "<a href='?page_id=".($page_id-1)."' class='button'>PREVIOUS</a>";
+                	}
+	                if($page_id!=$total){
+   	                    echo "<a href='?page_id=".($page_id+1)."' class='button'>NEXT</a>";
+	                }
 
-                echo "<ul class='page'>";
+                	echo "<ul class='page'>";
 
-                    for($i=1;$i<=$total;$i++)
-                {
-                      if($i==$page_id) { echo "<li class='active'>".$i."</li>"; }
+                    for($i=1;$i<=$total;$i++){
                       
-                      else { echo "<a href='?page_id=".$i."'>".$i."</a> "; }
-                  }
-	  	?>
+                      	if($i==$page_id) { 
+                      		echo "<li class='active'>".$i."</li>"; 
+                      	}else { 
+                      		echo "<a href='?page_id=".$i."'>".$i."</a> "; 
+                      	}
+                  	}
+	  			?>
 	 
-	  </ul>
-  </center>
-</div>
+	  		</ul>
+  		</center>
+	</div>
+	<!-- A row ends here -->
 
-<script>
-	function updateEventStudentInfoModal(id, name , mobile , email , cost) {		
-	    document.getElementById("updateEnumber").value = id;
-	    document.getElementById("updateEname").value = name;
-	    document.getElementById("updateEmobile").value = mobile;
-	    document.getElementById("updateEemail").value = email;
-	    document.getElementById("updateEcost").value = cost;
-	}
-	function updatePaginationField(){
+	<!-- We can add following Javascript into a separete js file -->
+	<!-- 
+		Following is a javascript which is used to display selected 
+		users information into the modal 
+	-->
+	<script>
+		
+		// This is function which is used to show users information 
+		// into the modal's sections 
+		function updateEventStudentInfoModal(id, name , mobile , email , cost) {		
+		    document.getElementById("updateEnumber").value = id;
+		    document.getElementById("updateEname").value = name;
+		    document.getElementById("updateEmobile").value = mobile;
+		    document.getElementById("updateEemail").value = email;
+		    document.getElementById("updateEcost").value = cost;
+		}
 
-	}
+	</script>
+	<!-- Script ends here -->
 
-</script>
-<!-- Modals for events -->
-<?php
-	include('modals.php');
-?>
+	<!-- All Modals for entire application -->	
+	<?php
+		include('modals.php');
+	?>
+	<!-- Addition of modals ends here  -->
+
+	<!-- College page ends here -->
