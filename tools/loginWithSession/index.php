df4d08ec6be62dfc4009387a3e9b5512d6b12174
upa8 <pratikupacharya@gmail.com>
2016-01-10 11:21:22 +0530

    Login with session

---
+++ tools/loginWithSession/index.php
@@ -0,0 +1,36 @@
+<html>
+	<head>
+		<title>User Login</title>
+		<link rel="stylesheet" type="text/css" href="styles.css" />
+	</head>
+	<body>
+		<form name="frmUser" method="post" action="login.php">
+			<div class="message"><?php if($message!="") { echo $message; } ?></div>
+				<table border="0" cellpadding="10" cellspacing="1" width="500" align="center">
+				<tr class="tableheader">
+				<td align="center" colspan="2">Enter Login Details</td>
+				</tr>
+				<tr class="tablerow">
+				<td align="right">Username</td>
+				<td><input type="text" name="userName"></td>
+				</tr>
+				<tr class="tablerow">
+				<td align="right">Password</td>
+				<td><input type="password" name="password"></td>
+				</tr>
+				<tr class="tableheader">
+				<td align="center" colspan="2"><input type="submit" name="submit" value="Submit"></td>
+				</tr>
+				</table>
+		</form>
+	</body>
+</html>
+
+
+
+$_SESSION["user_id"] = $row[user_id];
+session_start();
+
+// logout 
+unset($_SESSION["user_id"]);
+unset($_SESSION["user_name"]);
